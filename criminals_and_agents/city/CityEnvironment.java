8a95c5d715eee92573ec80501f6833e1094d768d
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2024-03-17 15:21:10 +0100

    CityModel and CityView

--- criminals_and_agents/city/CityEnvironment.java
+++ criminals_and_agents/city/CityEnvironment.java
@@ -6,4 +6,7 @@ import java.util.logging.*;
 
 public class CityEnvironment extends jason.environment.Environment {
     private Logger logger = Logger.getLogger("criminals_and_agents.mas2j." + CityEnvironment.class.getName());
+
+    CityModel city_model;
+    CityView city_view;
 }
