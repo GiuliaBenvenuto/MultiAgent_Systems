5a3d17fd51575225edf9486f2c65899f847c5616
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2024-03-17 14:59:56 +0100

    CityEnvironment.java

--- agents_and_criminals/src/env/CityEnvironment.java
+++
@@ -1,9 +0,0 @@
-import jason.asSyntax.*;
-import java.util.logging.*;
-import jason.environment.Environment;
-import jason.environment.grid.Location;
-
-public class CityEnvironment extends jason.environment.Environment {
-    
-    private Logger logger = Logger.getLogger("agents_and_criminals."+CityEnvironment.class.getName());
-}
---
+++ criminals_and_agents/city/CityEnvironment.java
@@ -0,0 +1,9 @@
+package city;
+
+import jason.asSyntax.*;
+import jason.environment.*;
+import java.util.logging.*;
+
+public class CityEnvironment extends jason.environment.Environment {
+    private Logger logger = Logger.getLogger("criminals_and_agents.mas2j." + CityEnvironment.class.getName());
+}
