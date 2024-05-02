1bf2731610bd754311475c4074470bd15abbeebe
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2024-05-02 08:56:06 +0200

    Stop internal action comments and code cleaning
    Took 6 minutes

--- criminals_and_agents/path/Stop.java
+++ criminals_and_agents/path/Stop.java
@@ -11,47 +11,40 @@ import city.*;
 import java.util.*;
 import helper.AgentPercept;
 
+/**
+ * ---------- Stop Internal Action ----------
+ * This class represents the internal action that handles the logic of stopping the police agent in the city when
+ * they arrive at the jail and all the criminals have been arrested.
+ */
 
 public class Stop extends DefaultInternalAction {
-
-    private static final long serialVersionUID = 1L;
 
     @Override
     public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
         // Check the number of arguments
         if (args.length != 2) {
-            throw new JasonException("This internal action requires one argument.");
+            throw new JasonException("This internal action requires two argument.");
         }
 
-        // Extract the police agent ID and the criminal agent ID
+        // Extract the police agent ID
         int policeId = (int)((NumberTerm) args[0]).solve();
         String name = ((Atom)args[1]).getFunctor();
 
         CityEnvironment env = CityEnvironment.getInstance();
         CityModel model = CityModel.getCityModel();
 
+        // Get the number of arrested criminals
         int numArrestedCriminals = model.getArrestedCriminals();
         System.out.println("****** Number of arrested criminals: " + numArrestedCriminals);
 
+        // Check if all criminals have been arrested
         if (numArrestedCriminals != 2) {
-            // All criminals have been arrested
-            // Stop all the agents calling the agent percept method
 
-
-        } else {
+        }
+        else {
+            // If all criminals have been arrested, destory all agents
             AgentPercept.destroyAllAgents(env);
         }
-
-
-        // string agentName
-        String agentName = "police" + (policeId + 1);
-        System.out.println("--------> Agent name: " + agentName);
-
-        // Access the CityEnvironment to call methods related to escorting
-
-
-
-
         return true;
     }
 }
